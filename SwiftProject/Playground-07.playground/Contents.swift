7e3afce3efd51bfe432fd368ed24ceb6f26b5183
caoyong <caoyong@shopexunit.com>
2018-08-26 23:38:11 +0800

    [代码功能]字符串删除和添加

--- SwiftProject/Playground-07.playground/Contents.swift
+++ SwiftProject/Playground-07.playground/Contents.swift
@@ -45,7 +45,7 @@ variableString += " and carriage"
 //constantString += " and another Highlander"
 // 这会报告一个编译错误 (compile-time error) - 常量字符串不可以被修改。
 
-//6 字符串是值类型: Swift 的String类型是值类型 创建新的字符串时 当进行赋值操作或者函数方法传递 会进行值的拷贝 任何情况下会对已有的字符串进行创建新的副本  swift 默认字符串拷贝的方式保证了在函数/方法中传递的是字符串的值。 很明显无论该值来自于哪里，都是您独自拥有的。 您可以确信传递的字符串不会被修改，除非你自己去修改它
+//6 字符串是值类型: Swift中的String类型是值类型 创建新的字符串时 当进行赋值操作或者函数方法传递 会进行值的拷贝 任何情况下会对已有的字符串进行创建新的副本,swift默认字符串拷贝的方式保证了在函数/方法中传递的是字符串的值,很明显无论该值来自于哪里，都是您独自拥有的。 您可以确信传递的字符串不会被修改，除非你自己去修改它
 
 //7 字符的使用
 for character in "hello"{
@@ -55,7 +55,7 @@ for character in "hello"{
     print(character)
 }
 
-//字符串可以通过传递一个值类型为字符的数组进行初始化
+//字符串可以通过字符的数组进行初始化
 let charS: [Character] = ["h","e","l","l","o"]
 let characterString = String(charS)
 print(characterString)
@@ -72,7 +72,6 @@ stringvalue_1.append("hello")
 characterValue.append(characterValue_1)
 
 //8 字符串插值 插值字符串中写在括号中的表达式不能包含非转义反斜杠 (\)，并且不能包含回车或换行符 不过，插值字符串可以包含其他字面量
-
 let count = 2
 print("this string count \(count)")
 
@@ -82,7 +81,11 @@ print("This string have character is \(stringCount.count) number")
 
 // 注意: 通过count属性返回的字符数量并不总是与包含相同字符的NSString的length属性相同。NSString的length属性是利用 UTF-16 表示的十六位代码单元数字，而不是 Unicode 可扩展的字符群集
 
-//10 “每一个字符串都有一个关联的索引index类型 这个index对应着每个character在字符串的位置”   使用startIndex属性可以获取一个String的第一个Character的索引。使用endIndex属性可以获取最后一个Character的"后一个位置的索引"。因此，endIndex属性不能作为一个字符串的有效下标。如果String是空串，startIndex和endIndex是相等的  调用 String 的 index(before:) 或 index(after:) 方法，可以立即得到前面或后面的一个索引。您还可以通过调用 index(_:offsetBy:) 方法来获取对应偏移量的索引，这种方式可以避免多次调用 index(before:) 或 index(after:) 方法
+/*10 “每一个字符串都有一个关联的索引index类型 这个index对应着每个character(字符)在字符串的位置,使用startIndex属性可以获取String中第一个Character的索引。使用endIndex属性可以获取String最后一个Character的"后一个位置的索引".所以endIndex属性不能作为一个字符串的有效下标。如果String是空串，startIndex和endIndex是相等的
+  10.1: String 的 index(before:) 得到前面或后面的一个索引
+  10.2: index(after:)  得到后一个索引
+  10.3: index(_:offsetBy:) 取对应偏移量的索引
+ */
 let greeting = "Guten Tag!"
 //startndex 获取character对应的index
 
@@ -92,13 +95,29 @@ greeting[greeting.index(before: greeting.endIndex)]
 greeting[greeting.index(greeting.startIndex, offsetBy: 2)]
 greeting[greeting.index(after: greeting.startIndex)]
 
-//11 indices: 会创建一个包含全部索引的范围(Range)，用来在一个字符串中访问单个字符
+/*11 indices: 会创建一个包含全部索引的范围(Range)，用来在一个字符串中访问单个字符(indices 同样适合使用在Array Dictionary  Set 中)
+ */
 for index in greeting.indices {
-   
     print("greeting character \(greeting[index])")
 }
 
-//注意:上述的方法同意适合使用在Array Dictionary  Set 中
+/*12 插入和删除
+ */
+var addString = "hello"
+//插入单个字符
+addString.insert("!", at: addString.endIndex)
+//插入一段字符串
+addString.insert(contentsOf: "word", at: addString.index(before: addString.endIndex))
+//移除单个字符
+addString.remove(at: addString.index(before: addString.endIndex))
+
+//根据range来移除子字符串
+let range = addString.index(addString.endIndex, offsetBy: -4)..<addString.endIndex
+
+addString .removeSubrange(range)
+
+
+
 
 
 
